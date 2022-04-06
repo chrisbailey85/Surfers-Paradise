c589dd20c09044e522ec36e2ef939392178003a3
Chris Bailey <chris85bailey@aol.co.uk>
2022-04-06 09:22:41 +0100

    first commit

---
+++ public/script.js
@@ -0,0 +1,28 @@
+const hamburger = document.querySelector('.hamburger')
+const faqParagraph = document.querySelectorAll('.faq__paragraph')
+const faqBtn = document.querySelectorAll('.faq__btn')
+const nav = document.querySelector('.nav')
+
+hamburger.addEventListener('click', () => {
+  nav.classList.toggle('open')
+  hamburger.classList.toggle('is-active')
+  nav.classList.contains('is-active')
+    ? (hamburger.ariaExpanded = true)
+    : (hamburger.ariaExpanded = false)
+})
+
+faqBtn.forEach((btn, idx) => {
+  btn.addEventListener('click', (e) => {
+    faqBtn.forEach((btn) => {
+      btn.classList.remove('active')
+    })
+    faqParagraph.forEach((faqPara) => {
+      faqPara.classList.remove('active')
+    })
+    e.target.classList.contains('active')
+      ? (e.target.ariaExpanded = false)
+      : (e.target.ariaExpanded = true)
+    e.target.classList.toggle('active')
+    faqParagraph[idx].classList.toggle('active')
+  })
+})
